6cea5c442ec742370405576ed0ea99ffccf33bfc
Dejan Petrov <dejan.petrov@dapmn.com>
2022-05-11 15:22:43 +0000

    Add new file

---
+++ src/ubuntu/install/picard/install_picard.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -ex
+
+echo "##### Install Packages #####"
+    add-apt-repository ppa:musicbrainz-developers/stable
+    apt update
+    apt install --no-install-recommends -y picard epiphany-browser
+
+echo "##### Clean Up #####"
+rm -rf \
+  /var/lib/apt/lists/* \
+  /var/tmp/*
+
+# Default settings and desktop icon
+cp "/usr/share/applications/MusicBrainz Picard.desktop" $HOME/Desktop/
+chmod +x "$HOME/Desktop/MusicBrainz Picard.desktop"
